ba9aad8563d2a2d9477f6142e44a7522cb263d65
Jeppe Krogh <kjej@aarhus.dk>
2023-01-16 12:46:06 +0100

    adding lazy load to stackable material

--- src/apps/loan-list/materials/stackable-material/stackable-material.tsx
+++ src/apps/loan-list/materials/stackable-material/stackable-material.tsx
@@ -6,6 +6,7 @@ import MaterialInfo from "./material-info";
 import fetchMaterial, { MaterialProps } from "../utils/material-fetch-hoc";
 import { LoanType } from "../../../../core/utils/types/loan-type";
 import fetchDigitalMaterial from "../utils/digital-material-fetch-hoc";
+import { useItemHasBeenVisible } from "../../../../core/utils/helpers/lazy-load";
 
 export interface StackableMaterialProps {
   loan: LoanType;
@@ -21,6 +22,7 @@ const StackableMaterial: FC<StackableMaterialProps & MaterialProps> = ({
   openDueDateModal,
   openLoanDetailsModal
 }) => {
+  const { itemRef, hasBeenVisible: showItem } = useItemHasBeenVisible();
   const [additionalMaterials] = useState(
     amountOfMaterialsWithDueDate ? amountOfMaterialsWithDueDate - 1 : 0
   );
@@ -36,39 +38,47 @@ const StackableMaterial: FC<StackableMaterialProps & MaterialProps> = ({
   }, [faust, identifier, openLoanDetailsModal]);
 
   return (
-    <div
-      className={`list-reservation my-32 ${
-        additionalMaterials > 0 ? "list-reservation--stacked" : ""
-      }`}
-    >
-      {material && (
-        <MaterialInfo
-          openDetailsModal={openLoanDetailsModalHandler}
-          periodical={periodical}
-          material={material}
-          isbnForCover={identifier || ""}
-        >
-          <AdditionalMaterialsButton
-            showOn="desktop"
-            openDueDateModal={() =>
-              openDueDateModal && dueDate && openDueDateModal(dueDate)
-            }
-            additionalMaterials={additionalMaterials}
-          />
-          <MaterialOverdueLink showOn="desktop" dueDate={dueDate} />
-        </MaterialInfo>
-      )}
-      <MaterialStatus loan={loan}>
-        <AdditionalMaterialsButton
-          showOn="mobile"
-          openDueDateModal={() =>
-            openDueDateModal && dueDate && openDueDateModal(dueDate)
-          }
-          additionalMaterials={additionalMaterials}
-        />
-        <MaterialOverdueLink showOn="mobile" dueDate={dueDate} />
-      </MaterialStatus>
-    </div>
+    // eslint-disable-next-line react/jsx-no-useless-fragment
+    <>
+      <div
+        ref={itemRef}
+        className={`list-reservation my-32 ${
+          additionalMaterials > 0 ? "list-reservation--stacked" : ""
+        }`}
+      >
+        {showItem && (
+          <>
+            {material && (
+              <MaterialInfo
+                openDetailsModal={openLoanDetailsModalHandler}
+                periodical={periodical}
+                material={material}
+                isbnForCover={identifier || ""}
+              >
+                <AdditionalMaterialsButton
+                  showOn="desktop"
+                  openDueDateModal={() =>
+                    openDueDateModal && dueDate && openDueDateModal(dueDate)
+                  }
+                  additionalMaterials={additionalMaterials}
+                />
+                <MaterialOverdueLink showOn="desktop" dueDate={dueDate} />
+              </MaterialInfo>
+            )}
+            <MaterialStatus loan={loan}>
+              <AdditionalMaterialsButton
+                showOn="mobile"
+                openDueDateModal={() =>
+                  openDueDateModal && dueDate && openDueDateModal(dueDate)
+                }
+                additionalMaterials={additionalMaterials}
+              />
+              <MaterialOverdueLink showOn="mobile" dueDate={dueDate} />
+            </MaterialStatus>
+          </>
+        )}
+      </div>
+    </>
   );
 };
 
