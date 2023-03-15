b04f01383010cca1282c4f96c9771091e7424b0b
Jeppe Krogh <kjej@aarhus.dk>
2023-03-15 10:27:16 +0100

    added config to control whether to allow removing ready reservations

--- src/apps/reservation-list/list/reservation-list.entry.tsx
+++ src/apps/reservation-list/list/reservation-list.entry.tsx
@@ -63,6 +63,7 @@ export interface ReservationListTextProps {
   reservationDetailsDateOfReservationTitleText: string;
   reservationDetailsReadyForLoanText: string;
   reservationDetailsRemoveDigitalReservationText: string;
+  reservationDetailAllowRemoveReadyReservationsConfig: boolean;
   deleteReservationModalHeaderText: string;
   deleteReservationModalDeleteQuestionText: string;
   deleteReservationModalNotRegrettableText: string;
